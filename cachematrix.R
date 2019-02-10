b0f14b881186a3068ebd1e0db387142bfe4965bb
Kacper Lichota <kacperlichota@googlemail.com>
2019-02-10 13:07:02 +0000

    Completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MaskecacheMatrix Creates list to get/set matrix and get/set cached/inverted matrix, 
+##cacheSolve Calculate inverted matrix, however checks in the first place if 
+## matrix has been cached, if yes returns cached one. If not 
+## calculate inverted matrix and caches it.
 
-## Write a short comment describing this function
+## Creates list to get/set matrix and get/set cached/inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  set_inv <- function(inv) i <<- inv
+  get_inv <- function() i
+  list(set = set,
+       get = get,
+       set_inv = set_inv,
+       get_inv = get_inv)
+
 
 }
 
 
-## Write a short comment describing this function
+## Calculate inverted matrix, however checks in the first place if 
+## matrix has been cached, if yes returns cached one. If not 
+## calculate inverted matrix and caches it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$get_inv()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  mat <- x$get()
+  i <- solve(mat, ...)
+  x$set_inv(i)
+  i
+  
+  
+  
 }
